3b9c2c5bd75d1d2e464a302ae14de198c12710ac
cesonu <chiadi.esonu@gmail.com>
2024-12-10 00:56:36 +0000

    first commit

---
+++ src/Components/Context/StoreContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useState } from "react";
+import { food_list } from "../../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        const itemInfo = food_list.find((product) => product._id === String(item));
+        if (!itemInfo) {
+          console.error(`Item with ID ${item} not found in food_list`);
+          continue; // Skip this item if it doesn't exist in food_list
+        }
+        totalAmount += (itemInfo.price || 0) * cartItems[item]; 
+      }
+    }
+    return totalAmount;
+  };
+  
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems, 
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
